8fa5780423096e38822a8b7e7a9df3eafcdd9d60
Royall Spence <royall@royall.us>
2016-11-19 15:58:31 -0500

    Add AttributeSet

---
+++ src/Set/AttributeSet.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Set;
+
+use RoyallTheFourth\HtmlDocument\Attribute\AttributeInterface;
+
+final class AttributeSet implements SetInterface
+{
+    private $attributes = [];
+
+    public function __construct(AttributeInterface $attribute = null)
+    {
+        if ($attribute) {
+            $this->attributes[] = $attribute;
+        }
+    }
+
+    public function iterate(): \Generator
+    {
+        foreach ($this->attributes as $attribute) {
+            yield $attribute;
+        }
+    }
+
+    /**
+     * @param AttributeInterface $attribute
+     * @return AttributeSet
+     */
+    public function add($attribute): AttributeSet
+    {
+        $this->attributes[] = $attribute;
+        return $this;
+    }
+
+    /**
+     * @param $set AttributeSet
+     * @return AttributeSet
+     */
+    public function merge($set): AttributeSet
+    {
+        foreach ($set->iterate() as $attribute) {
+            $this->add($attribute);
+        }
+
+        return $this;
+    }
+}
---
+++ tests/Set/AttributeSetTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Set;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+
+final class AttributeSetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddAndIterate()
+    {
+        $set = new AttributeSet();
+        $set->add(new StandardAttribute('class', 'active'));
+        $set->add(new BooleanAttribute('required'));
+
+        $gen = $set->iterate();
+
+        static::assertEquals('class="active"', $gen->current()->render());
+        $gen->next();
+        static::assertEquals('required', $gen->current()->render());
+    }
+
+    public function testMerge()
+    {
+        $set1 = new AttributeSet(new StandardAttribute('class', 'active'));
+        $set2 = new AttributeSet(new BooleanAttribute('required'));
+
+        $set1->merge($set2);
+
+        $gen = $set1->iterate();
+
+        static::assertEquals('class="active"', $gen->current()->render());
+        $gen->next();
+        static::assertEquals('required', $gen->current()->render());
+    }
+}
