2f6f277801837806f9cfc310c1a055b9f5669e1f
Royall Spence <royall@royall.us>
2016-11-19 19:50:13 -0500

    Add BiDirectionalIsolation element type

---
+++ src/Element/BiDirectionalIsolation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+use RoyallTheFourth\HtmlDocument\Set\ElementSet;
+
+/**
+ * Class BiDirectionalIsolation
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/bdi
+ */
+final class BiDirectionalIsolation extends AbstractElement
+{
+    public function __construct(AttributeSet $attributes = null, ElementSet $children = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+        $this->children = $children ?? new ElementSet();
+    }
+
+    public function render(): string
+    {
+        $attributes = $this->renderAttributes();
+        $children = $this->renderChildren();
+
+        return "<bdi{$attributes}>{$children}\n</bdi>\n";
+    }
+
+    public function withAttribute(string $name, string $value = null): BiDirectionalIsolation
+    {
+        if($value) {
+            $attribute = new StandardAttribute($name, $value);
+        }else{
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new BiDirectionalIsolation($this->attributes->add($attribute));
+    }
+
+    public function withChild(ElementInterface $element): BiDirectionalIsolation
+    {
+        return new BiDirectionalIsolation($this->attributes, $this->children->add($element));
+    }
+}
