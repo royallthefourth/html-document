f82e7f6f89e16c8994012f8aa23ce369736c5f47
Royall Spence <royall@royall.us>
2016-11-19 20:16:54 -0500

    Add Canvas element type

---
+++ src/Element/Canvas.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+use RoyallTheFourth\HtmlDocument\Set\ElementSet;
+
+/**
+ * Class Canvas
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas
+ */
+final class Canvas extends AbstractElement
+{
+    public function __construct(AttributeSet $attributes = null, ElementSet $children = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+        $this->children = $children ?? new ElementSet();
+    }
+
+    public function render(): string
+    {
+        $attributes = $this->renderAttributes();
+        $children = $this->renderChildren();
+
+        return "<canvas{$attributes}>{$children}\n</canvas>\n";
+    }
+
+    public function withAttribute(string $name, string $value = null): Canvas
+    {
+        if($value) {
+            $attribute = new StandardAttribute($name, $value);
+        }else{
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new Canvas($this->attributes->add($attribute));
+    }
+
+    public function withChild(ElementInterface $element): Canvas
+    {
+        return new Canvas($this->attributes, $this->children->add($element));
+    }
+
+    public function withHeight($height): Canvas
+    {
+        return $this->withAttribute('height', $height);
+    }
+
+    public function withWidth($width): Canvas
+    {
+        return $this->withAttribute('width', $width);
+    }
+}
