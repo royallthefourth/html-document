7cb75fcaf87eb468198623615141c8afbcffd585
Royall Spence <royall@royall.us>
2016-11-19 20:40:01 -0500

    Add DataList element type

---
+++ src/Element/DataList.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+use RoyallTheFourth\HtmlDocument\Set\ElementSet;
+
+/**
+ * Class DataList
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist
+ */
+final class DataList extends AbstractElement
+{
+    public function __construct(AttributeSet $attributes = null, ElementSet $children = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+        $this->children = $children ?? new ElementSet();
+    }
+
+    public function render(): string
+    {
+        $attributes = $this->renderAttributes();
+        $children = $this->renderChildren();
+
+        return "<datalist{$attributes}>{$children}\n</datalist>\n";
+    }
+
+    public function withAttribute(string $name, string $value = null): DataList
+    {
+        if ($value) {
+            $attribute = new StandardAttribute($name, $value);
+        } else {
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new DataList($this->attributes->add($attribute));
+    }
+
+    public function withChild(ElementInterface $element): DataList
+    {
+        return new DataList($this->attributes, $this->children->add($element));
+    }
+}
