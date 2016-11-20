e0c91808f32153ba6ffee2e189099486bcddcf05
Royall Spence <royall@royall.us>
2016-11-19 20:23:09 -0500

    Add Code element type

---
+++ src/Element/Code.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+use RoyallTheFourth\HtmlDocument\Set\ElementSet;
+
+/**
+ * Class Code
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/code
+ */
+final class Code extends AbstractElement
+{
+    public function __construct(AttributeSet $attributes = null, ElementSet $children = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+        $this->children = $children ?? new ElementSet();
+    }
+
+    public function render(): string
+    {
+        $attributes = $this->renderAttributes();
+        $children = $this->renderChildren();
+
+        return "<code{$attributes}>{$children}\n</code>\n";
+    }
+
+    public function withAttribute(string $name, string $value = null): Code
+    {
+        if($value) {
+            $attribute = new StandardAttribute($name, $value);
+        }else{
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new Code($this->attributes->add($attribute));
+    }
+
+    public function withChild(ElementInterface $element): Code
+    {
+        return new Code($this->attributes, $this->children->add($element));
+    }
+}
