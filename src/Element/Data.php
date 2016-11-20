7035b4798e2e24b1343acd6679696f8513fbe979
Royall Spence <royall@royall.us>
2016-11-19 20:35:01 -0500

    Add Data element type

---
+++ src/Element/Data.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+use RoyallTheFourth\HtmlDocument\Set\ElementSet;
+
+/**
+ * Class Data
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/data
+ */
+final class Data extends AbstractElement
+{
+    public function __construct(AttributeSet $attributes = null, ElementSet $children = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+        $this->children = $children ?? new ElementSet();
+    }
+
+    public function render(): string
+    {
+        $attributes = $this->renderAttributes();
+        $children = $this->renderChildren();
+
+        return "<data{$attributes}>{$children}\n</data>\n";
+    }
+
+    public function withAttribute(string $name, string $value = null): Data
+    {
+        if ($value) {
+            $attribute = new StandardAttribute($name, $value);
+        } else {
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new Data($this->attributes->add($attribute));
+    }
+
+    public function withChild(ElementInterface $element): Data
+    {
+        return new Data($this->attributes, $this->children->add($element));
+    }
+
+    public function withValue(string $value): Data
+    {
+        return $this->withAttribute('value', $value);
+    }
+}
