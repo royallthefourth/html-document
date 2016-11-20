c733c6e3175aa9dd4ba9573d2953666b8a0ad38b
Royall Spence <royall@royall.us>
2016-11-19 20:03:27 -0500

    Add Br element type

---
+++ src/Element/Br.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+
+/**
+ * Class Br
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/br
+ */
+final class Br extends AbstractElement
+{
+    public function __construct(AttributeSet $attributes = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+    }
+
+    public function render(): string
+    {
+        $attributes = $this->renderAttributes();
+
+        return "<br{$attributes}>\n";
+    }
+
+    public function withAttribute(string $name, string $value = null): Br
+    {
+        if($value) {
+            $attribute = new StandardAttribute($name, $value);
+        }else{
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new Br($this->attributes->add($attribute));
+    }
+
+    /**
+     * Does nothing.
+     * @param ElementInterface $element
+     * @return Br
+     */
+    public function withChild(ElementInterface $element): Br
+    {
+        return $this;
+    }
+}
