61e84cb9ffb881129032ef1a6fa19bfab5ac51e2
Royall Spence <royall@royall.us>
2016-11-20 20:04:34 -0500

    Add Title element

---
+++ src/Element/Title.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RoyallTheFourth\HtmlDocument\Element;
+
+use RoyallTheFourth\HtmlDocument\Attribute\BooleanAttribute;
+use RoyallTheFourth\HtmlDocument\Attribute\StandardAttribute;
+use RoyallTheFourth\HtmlDocument\Set\AttributeSet;
+use RoyallTheFourth\HtmlDocument\Set\ElementSet;
+use RoyallTheFourth\HtmlDocument\Tag\Standard;
+
+/**
+ * Class Title
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/title
+ */
+final class Title extends AbstractElement implements ParentElementInterface
+{
+    public function __construct(AttributeSet $attributes = null, ElementSet $children = null)
+    {
+        $this->attributes = $attributes ?? new AttributeSet();
+        $this->children = $children ?? new ElementSet();
+        $this->tag = new Standard('title', $attributes, $children);
+    }
+
+    public function withAttribute(string $name, string $value = null): Title
+    {
+        if ($value) {
+            $attribute = new StandardAttribute($name, $value);
+        } else {
+            $attribute = new BooleanAttribute($name);
+        }
+
+        return new Title($this->attributes->add($attribute), $this->children);
+    }
+
+    public function withChild(ElementInterface $element): Title
+    {
+        return new Title($this->attributes, $this->children->add($element));
+    }
+}
